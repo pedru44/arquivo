d0d39257022cc63182e38278ef863ca42fb6a151
pedru44 <pedroc@iesb.edu.br>
2024-02-26 21:45:49 -0300

    java script

---
+++ script/script.js
@@ -0,0 +1,19 @@
+
+
+var num = 1;
+num = 3;
+var prince = 1.5
+var myName = 'Pedro';
+var trueValue = true;
+var nullVar = null;
+var und;
+
+console.log('num:' + num);
+console.log('Meu nome' + myName);
+console.log('prince:' + prince);
+console.log('trueValue:' + trueValue);
+console.log('nullVar:' + nullVar);
+console.log('und:' + und);
+
+
+
